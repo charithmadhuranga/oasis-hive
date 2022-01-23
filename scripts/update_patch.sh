dbda6ccc21e2e7f8cfdb6e51a37b1619585fb689
Oasis-X <71035180+oasis-main@users.noreply.github.com>
2022-01-23 17:31:35 -0500

    Update update_patch.sh

--- scripts/update_patch.sh
+++ scripts/update_patch.sh
@@ -2,5 +2,5 @@
 
 echo "Running shell commands to update system environment..."
 . /home/pi/oasis-hive/scripts/setup_config.sh
-. /home/pi/oasis-hive/scripts/optimize_boot.sh -no_bt
+. /home/pi/oasis-hive/scripts/optimize_boot.sh
 
